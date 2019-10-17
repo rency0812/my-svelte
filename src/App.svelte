a4b6e99c9ea26466942d21cc6efea45da4d2deb6
任立 <renl@fadada.com>
2019-10-17 14:20:20 +0800

    提交修改

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,10 @@
 <script>
 	export let name;
+	let src = 'http://pic39.nipic.com/20140307/13928177_195158772185_2.jpg';
+	let str = `this is a <b>svelte</b> project`;
+	function getName () {
+		alert(name)
+	}
 </script>
 
 <style>
@@ -9,3 +14,5 @@
 </style>
 
 <h1>Hello {name}!</h1>
+<img on:click="{getName}" {src} alt="{name}" />
+<div>{@html str}</div>
